5b25ffc804d0395df2b6768fdf2a1e879d08f6c4
Janet <janetwong024@gmail.com>
2022-07-31 14:05:37 -0400

    updated real time time/date

--- src/app.js
+++ src/app.js
@@ -14,3 +14,28 @@ function displayTemp(response) {
 let apiKey = "e37af8b7e5570ed34e9bd67d3c3d2d12";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
 axios.get(apiUrl).then(displayTemp);
+
+let now = new Date();
+let dateElement = document.querySelector("#date");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+dateElement.innerHTML = `${day} ${hours}:${minutes}`;
