fe08e73b54811fee4e5365928b6a108968316821
Janet <janetwong024@gmail.com>
2022-08-01 19:52:44 -0400

    added in form html

--- src/app.js
+++ src/app.js
@@ -1,26 +1,3 @@
-function displayTemp(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let conditionElement = document.querySelector("#condition");
-  let maxTempElement = document.querySelector("#max-temp");
-  let minTempElement = document.querySelector("#min-temp");
-  let weatherIconElement = document.querySelector("#icon");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.data.name;
-  conditionElement.innerHTML = response.data.weather[0].description;
-  maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
-  minTempElement.innerHTML = Math.round(response.data.main.temp_min);
-  weatherIconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  weatherIconElement.setAttribute("alt", response.data.weather[0].description);
-}
-let apiKey = "e37af8b7e5570ed34e9bd67d3c3d2d12";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-axios.get(apiUrl).then(displayTemp);
-
 let now = new Date();
 let dateElement = document.querySelector("#date");
 let days = [
@@ -45,3 +22,27 @@ if (minutes < 10) {
 }
 
 dateElement.innerHTML = `${day} ${hours}:${minutes}`;
+
+function displayTemp(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let conditionElement = document.querySelector("#condition");
+  let maxTempElement = document.querySelector("#max-temp");
+  let minTempElement = document.querySelector("#min-temp");
+  let weatherIconElement = document.querySelector("#icon");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  conditionElement.innerHTML = response.data.weather[0].description;
+  maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
+  minTempElement.innerHTML = Math.round(response.data.main.temp_min);
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  weatherIconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
+let apiKey = "e37af8b7e5570ed34e9bd67d3c3d2d12";
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+axios.get(apiUrl).then(displayTemp);
